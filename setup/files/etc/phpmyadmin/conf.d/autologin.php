36d7ace1e2337483b43e74e8ed0f1919d84a0c66
Léo Benoist <leo@uniiloveu.com>
2014-07-01 15:56:55 -0400

    Phpmyadmin autologin

---
+++ setup/files/etc/phpmyadmin/conf.d/autologin.php
@@ -0,0 +1,9 @@
+<?php
+
+/* Authentication type */
+$cfg['Servers'][$i]['auth_type'] = 'config';
+/* Server parameters */
+$cfg['Servers'][$i]['host'] = 'localhost';
+/* User for advanced features */
+$cfg['Servers'][$i]['controluser'] = 'root';
+$cfg['Servers'][$i]['controlpass'] = 'root';
