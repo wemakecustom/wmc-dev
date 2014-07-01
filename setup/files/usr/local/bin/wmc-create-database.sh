cade914aa927c487dc0090bcf0d189c520438dc2
Sébastien Lavoie <github@lavoie.sl>
2014-07-01 16:53:14 -0400

    fixed wmc-create-database.sh

--- setup/files/usr/local/bin/wmc-create-database.sh
+++ setup/files/usr/local/bin/wmc-create-database.sh
@@ -10,12 +10,11 @@ if [[ "$(pwd)" =~ $regex ]]; then
 
     echo "Doing setup for database '${database}'"
 
-    mysql -BNe "SHOW DATABASES LIKE '${database}'" || mysql -e "CREATE DATABASE ${database};"
+    [ -n "$(mysql -BNe "SHOW DATABASES LIKE '${database}'")" ] || mysql -e "CREATE DATABASE ${database};"
     echo "host='localhost'
 user='root'
 pass='root'
-name='${database}'
-" > confs/database.ini
+name='${database}'" > confs/database.ini
 else
     echo "Unable to guess database name for $(pwd)" >&2
     exit 1
